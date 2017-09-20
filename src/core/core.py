e147552ad37b5d6a8357ddd83e9c9683fc2b4ed6
Yongho Kim <ironyonghokim@gmail.com>
2017-09-20 00:38:48 -0500

    Added core and comm modules

---
+++ src/core/core.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+"""
+    This is a core module that supports fundamental componets to run simulation.
+"""
+
+import logging
+
+from sc2_comm import sc2
+
+FORMAT = '%(asctime)s %(levelname)s %(lineno)d %(message)s'
+logging.basicConfig(level=logging.INFO, format=FORMAT)
+logger = logging.getLogger(__name__)
+
+class Core(object):
+    def __init__(self):
+        self.comm = sc2()
+
+    def init(self):
+        self.comm.open()
+
+    def deinit(self):
+        pass
+
+    def run(self):
+        pass
+
+
+if __name__ == '__main__':
+    core = Core()
+    logger.info('Core initializing...')
+    core.init()
+    logger.info('Core running...')
+    core.run()
+    logger.info('Core deinitializing...')
+    core.deinit()
+    logger.info('Core terminated.')
