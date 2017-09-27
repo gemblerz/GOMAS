76e33995365297cd6ba4739ba8bcf5c922a39462
parksanguk <psu9808@naver.com>
2017-09-26 22:50:37 -0400

    gor-purdue: append the code that imports sys module and adds path.
    Error occurred because game.py cannot import other python files which are on diff dir.
    So, just add path using sys module.

--- src/examples/game.py
+++ src/examples/game.py
@@ -3,6 +3,9 @@ from s2clientprotocol import raw_pb2 as raw_pb
 from s2clientprotocol import spatial_pb2 as spatial
 from s2clientprotocol import common_pb2 as common
 from websocket import create_connection
+
+import sys
+sys.path.append('../core')
 from sc2_comm import sc2
 from core import Core
 
@@ -19,7 +22,7 @@ map_info = sc_pb.LocalMap()
 #Windows
 # map_info.map_path = "C:\Program Files (x86)\StarCraft II\Maps\Melee\Simple128.SC2Map"
 #Mac
-# map_info.map_path = "/Applications/StarCraft II/Maps/Melee/Simple128.SC2Map"
+map_info.map_path = "/Applications/StarCraft II/Maps/Melee/Simple128.SC2Map"
 create_game = sc_pb.RequestCreateGame(local_map = map_info)
 create_game.player_setup.add(type=1)
 create_game.player_setup.add(type=2)
