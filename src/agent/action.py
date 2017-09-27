514a134c37c8bbdd1f75d1337063d4d94da5b45f
Yongho Kim <ironyonghokim@gmail.com>
2017-09-27 09:41:30 -0500

    working on agent model

---
+++ src/agent/action.py
@@ -0,0 +1,8 @@
+"""
+    Action class
+    This lists all possible actions of a unit
+    Actions can be performed under conditions and events
+"""
+
+class Action(object):
+    def __init__(self):
---
+++ src/agent/agent.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""
+    Class Agent
+    This contains core components,
+        - knowledge: manages statements (e.g., I am at (x, y))
+        - action: lists possible actions
+        - goal: explains details of goals
+        - reasoning: the brain
+        - comm: ability to communicate with others
+"""
+
+class Agent(object):
+    def __init__(self):
+        pass
+
+    def spawn(self, dict_args):
+        assert 'actions' in dict_args
+        
+
+    '''
+        Main logic runs here (i.e., reasoning)
+    '''
+    def run(self):
+        while True:
+            pass
