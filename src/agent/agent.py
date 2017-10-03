b8f6affe324766e9deca3e14fd07c51648276384
Yongho Kim <ironyonghokim@gmail.com>
2017-10-03 00:26:48 -0500

    Re-design arch.

--- src/agent/agent.py
+++ src/agent/agent.py
@@ -15,9 +15,9 @@ import logging
 sys.path.append('../')
 from units import units
 
-from action import Action
+from action import Action, get_basic_actions
 from knowledge_base import Knowledge
-from goal import Goal
+from goal import Goal, create_goal_set
 
 FORMAT = '%(asctime)s %(module)s %(levelname)s %(lineno)d %(message)s'
 logging.basicConfig(level=logging.INFO, format=FORMAT)
@@ -29,14 +29,16 @@ class Agent(object):
         self.discrete_time_step = 1 # sec
         self.alive = False
 
-        self.actions = []
+        self.actions = get_basic_actions()
+        self.knowledge = []
+        self.goals = []
 
     def _load_knowledge(self, knowledge):
         for k in knowledge:
-            self.actions.append(Knowledge(k))
+            self.knowledge.append(Knowledge(k))
 
     def _load_goals(self, goals):
-        pass
+        self.goals = goals
 
     def spawn(self, unit_id, initial_knowledge=[], initial_goals=[]):
         assert unit_id in units
@@ -45,10 +47,10 @@ class Agent(object):
         self.load_unit(units[unit_id])
 
         # Set initial statements in knowledge
-        self.knowledge = self._load_knowledge(initial_knowledge)
+        self._load_knowledge(initial_knowledge)
 
         # Store initial goals
-        self.goals = Goal(initial_goals)
+        self._load_goals(initial_goals)
 
         # Initialize communications
         self.init_comm_env()
@@ -61,7 +63,17 @@ class Agent(object):
         self.id = spec['id']
         self.name = spec['name']
 
-        self.actions = Action(spec['actions'])
+        for action in spec['actions']:
+            assert 'id' in action
+            assert 'name' in action
+            assert 'require' in action
+
+            self.actions.append(
+                Action(
+                    action_name=action['name'],
+                    actual_code='',
+                    require=action['require'],
+                    sc_action_id=action['id']))
 
     '''
         Communication to simulator
@@ -95,7 +107,9 @@ class Agent(object):
         Information / actions going to simulator
     '''
     def act(self, action):
-        pass
+        logger.info('%s is performing %s' % (self.name, action))
+        action.perform()
+        return True
 
     '''
         Delivering information to other agents
@@ -110,29 +124,48 @@ class Agent(object):
         pass
 
     '''
+        Returns an action that can perform the task
+    '''
+    def _has_action_for_task(self, task):
+        for action in self.actions:
+            if action.can_perform(task.__name__):
+                action.set_arguments(task.arguments)
+                return action
+        return None
+
+    '''
         Returns available actions based on the desires in the current situation
     '''
     def next_action(self, current_goal, current_knowledge):
         list_actions = []
-        if current_goal is not None:
-            goal, required_action = current_goal
+        if len(current_goal) == 0:
+            # TODO: is an action always triggered by a goal?
+            return None, None
 
-            # If the agent know who to attain the goal
-            
-
+        # TODO: all the goals may need to be examined
+        g = current_goal.pop()
+        logger.info('current goal is %s' % (g,))
 
         # List up possible actions that can achieve the goal
+        # If the agent knows how to attain the goal
+        for task in g.get_tasks():
+            action = self._has_action_for_task(task)
+            if action is not None:
+                list_actions.append(action)
 
         # Select actions from the list of actions in terms of the current situation
+        return_action = list_actions[0]
 
         # Return the most beneficial action from the selected actions
-        pass
+        return return_action, g
 
     '''
         Main logic runs here (i.e., reasoning)
     '''
     def run(self):
         while self.alive:
+            # For debugging
+            logger.info('%s is ticking' % (self.name,))
             # Check if something to answer
             # query = self.check_being_asked():
             # if query:
@@ -142,13 +175,18 @@ class Agent(object):
             # self.perceive()
 
             # Reason next action
-            action = self.next_action(self.goals, self.knowledge)
+            selected_action, selected_goal = self.next_action(self.goals, self.knowledge)
 
             # Perform the action
-            self.act(action)
+            if selected_action is not None:
+                if not self.act(selected_action):
+                    # Action failed, put the goal back to the queue
+                    self.goals.append(selected_goal)
 
             # May need to tell others the action that is about to be performed
             # self.tell('%d performs %s' % (self.id, action))
+            # Or
+            # May tell others the action has performed
 
             # Sleep a while to prevent meaningless burst looping
             time.sleep(self.discrete_time_step)
@@ -168,7 +206,7 @@ if __name__ == '__main__':
             ('type2', 'i', 'say', ['my_name']),
             ],
         initial_goals=[
-            ({'goal': 'say hello', 'require': ['say', 'hello']}),
+            create_goal_set({'goal': 'say hello', 'require': [['say', {'words':'hello'}]]}),
             ]
         )
     print('Agent is running...')
