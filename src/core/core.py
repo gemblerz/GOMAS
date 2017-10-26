227b236b7f2a91a3ecda740384d95ea6c4761c0e
parksanguk <psu9808@naver.com>
2017-10-26 13:54:56 -0400

    gor-purdue: refactor core.py

--- src/core/core.py
+++ src/core/core.py
@@ -14,8 +14,11 @@ sys.path.append('../agent')
 from agent import Agent
 from goal import Goal, create_goal_set
 
+from units import units
+
 from sc2_comm import sc2
 from s2clientprotocol import sc2api_pb2 as sc_pb
+from s2clientprotocol import raw_pb2 as raw_pb
 
 FORMAT = '%(asctime)s %(module)s %(levelname)s %(lineno)d %(message)s'
 logging.basicConfig(level=logging.INFO, format=FORMAT)
@@ -28,7 +31,9 @@ class Core(object):
         self.port = 5000
 
         if sys.platform == "darwin": # Mac OS X
-            self.launcher_path = "/Applications/StarCraft\ II/Support/SC2Switcher.app/Contents/MacOS/SC2Switcher --listen 127.0.0.1 --port %s"%self.port
+            self.launcher_path = "/Applications/StarCraft\ II/Support/SC2Switcher.app/Contents/MacOS/SC2Switcher\
+                                  --listen 127.0.0.1\
+                                  --port %s"%self.port
             self.map_path = "/Applications/StarCraft II/Maps/Melee/Simple128.SC2Map"
 
         elif sys.platform == "win32": # Windows
@@ -56,56 +61,70 @@ class Core(object):
     def deinit(self):
         pass
 
-    def _create_new_game(self):
+    def _start_new_game(self):
+
         # create a game
+        try:
+            map_info = sc_pb.LocalMap()
 
-        map_info = sc_pb.LocalMap()
-        # Windows
-        # map_info.map_path = "C:\Program Files (x86)\StarCraft II\Maps\Melee\Simple128.SC2Map"
-        # Mac
-        map_info.map_path = self.map_path
-        create_game = sc_pb.RequestCreateGame(local_map=map_info)
-        create_game.player_setup.add(type=1)
-        create_game.player_setup.add(type=2)
+            map_info.map_path = self.map_path
+            create_game = sc_pb.RequestCreateGame(local_map=map_info)
+            create_game.player_setup.add(type=1)
+            create_game.player_setup.add(type=2)
 
-        create_game.realtime = True
-        # print("Make")
-        # send Request
-        print(self.comm.send(create_game=create_game))
-        # print (test_client.comm.read())
+            create_game.realtime = True
 
-        logger.info('Game created.')
+            # send Request
+            self.comm.send(create_game=create_game)
+            # print (test_client.comm.read())
+
+            logger.info('New game is created.')
+        except Exception as ex:
+            logger.error('While creating a new game: %s'%str(ex))
+
+        # join the game
+        try:
+            interface_options = sc_pb.InterfaceOptions(raw=True, score=True)
+            join_game = sc_pb.RequestJoinGame(race=3, options=interface_options)
+
+            # send Request
+            self.comm.send(join_game=join_game)
+
+            logger.info('Success to join the game.')
+        except Exception as ex:
+            logger.error('While joining the game: %s'%str(ex))
+
+        # Game Start
+        try:
+            self.comm.send(step=sc_pb.RequestStep(count=1))
+
+            logger.info('Game is Started.')
+        except Exception as ex:
+            logger.error('While starting a new game: %s'%str(ex))
+
 
     def run(self):
 
-        self._create_new_game()
-        """Join Game"""
-        # Make Requst(JoinGame)
+        self._start_new_game()
 
-        interface_options = sc_pb.InterfaceOptions(raw=True, score=True)
-        join_game = sc_pb.RequestJoinGame(race=3, options=interface_options)
-
-        # send Request
-        self.comm.send(join_game=join_game)
-        # print (test_client.comm.read())
-
-        # Game Start
-        self.comm.send(step=sc_pb.RequestStep(count=1))
-        # print (test_client.comm.read())
-
-        logger.info('Game Started.')
-
-
-        unit_tag_list = []
+        list_unit_tag = []
         observation = sc_pb.RequestObservation()
         t = self.comm.send(observation=observation)
 
         for unit in t.observation.observation.raw_data.units:
             if unit.unit_type == 84:  # Probe unit_type_tag
-                unit_tag_list.append(unit.tag)
+                list_unit_tag.append(unit.tag)
+
+        action=raw_pb.ActionRawUnitCommand(ability_id=4)
+        action.unit_tags.append(list_unit_tag[0])
+        action_raw = raw_pb.ActionRaw(unit_command=action)
+
+        action = sc_pb.RequestAction()
+        action.actions.add(action_raw=action_raw)
+        self.comm.send(action=action)
 
         probe = Agent()
-        probe.spawn(unit_tag_list[0], 84,
+        probe.spawn(list_unit_tag[0], 84,
                     initial_knowledge=[
                         ('type1', 'my_name', ['probe']),
                         ('type2', 'i', 'say', ['my_name']),
