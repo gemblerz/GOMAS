18a47820f74a7c0486bfcb93aace78ab20b32c5f
hyeonggunlee <gudrjs012@gmail.com>
2017-10-26 01:43:14 -0400

    added test_get_goal() which test get_goal() function

--- src/agent/test_goal.py
+++ src/agent/test_goal.py
@@ -24,5 +24,31 @@ class GoalModelTest(unittest.TestCase):
         expected_result = Task('say', {'words':'hajime'})
         self.assertEqual(expected_result.arguments, result[0].arguments)
 
+    # test the get_goal() function which returns its first subgoals(dependents[0]).
+    def test_get_goal(self):
+        goal = {'goal': 'introduce myself',
+             'require': [
+                ['say', {'words':'hello'}],
+                {'goal': 'say hello',
+                 'require': [
+                    ['say', {'words':'myname'}],
+                    ['say', {'words':'hehe'}],
+                     {'goal': 'say hajime',
+                      'require': [
+                        ['say', {'words':'hajime'}]
+                       ]}
+                  ]
+                }
+             ]
+        }
+        model = create_goal_set(goal)
+        result = model.get_goal().name
+        expected_result = "say hello"
+        self.assertEqual(expected_result, result) # checked the goal's subgoal name("say hello")
+
+
+
+
+
 if __name__ == '__main__':
     unittest.main()
