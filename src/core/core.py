0a9dea9523234bf20a7ca28c1a6fa9fe8759db7f
hyeonggunlee <gudrjs012@gmail.com>
2017-11-30 16:22:57 -0500

    build two pylon after gathering minerals

--- src/core/core.py
+++ src/core/core.py
@@ -10,6 +10,7 @@ import os
 import time
 import sys
 import json
+
 sys.path.append('../agent')
 from agent import Agent
 from goal import Goal, create_goal_set
@@ -26,22 +27,22 @@ FORMAT = '%(asctime)s %(module)s %(levelname)s %(lineno)d %(message)s'
 logging.basicConfig(level=logging.INFO, format=FORMAT)
 logger = logging.getLogger(__name__)
 
-class Core(object):
 
+class Core(object):
     def __init__(self):
         self.comm_sc2 = sc2()
         self.port = 5000
 
-        if sys.platform == "darwin": # Mac OS X
+        if sys.platform == "darwin":  # Mac OS X
             self.launcher_path = "/Applications/StarCraft\ II/Support/SC2Switcher.app/Contents/MacOS/SC2Switcher\
                                   --listen 127.0.0.1\
-                                  --port %s"%self.port
-            self.map_path = os.getcwd()+'/../../resource/Maps/GorasMap_GG.SC2Map'
+                                  --port %s" % self.port
+            self.map_path = os.getcwd() + '/../../resource/Maps/GorasMap.SC2Map'
 
-        elif sys.platform == "win32": # Windows
+        elif sys.platform == "win32":  # Windows
 
-            self.launcher_path = 'C:\\"Program Files (x86)"\\"StarCraft II"\\"Support"\SC2Switcher.exe --listen 127.0.0.1 --port %s"'%self.port
-            self.map_path = os.getcwd()+'/../../resource/Maps/GorasMap_GG.SC2Map'
+            self.launcher_path = 'C:\\"Program Files (x86)"\\"StarCraft II"\\"Support"\SC2Switcher.exe --listen 127.0.0.1 --port %s"' % self.port
+            self.map_path = os.getcwd() + '/../../resource/Maps/GorasMap.SC2Map'
 
         else:
             logger.error("Sorry, we cannot start on your OS.")
@@ -54,13 +55,13 @@ class Core(object):
         self.threads_agents = []
 
         # for test two agent sys
-        self.spawned_agent=0
+        self.spawned_agent = 0
 
         # Set the dictionary to save the information from SC2 client.
         self.dict_probe = {}
         self.dict_mineral = {}
         self.dict_nexus = {}
-        self.next_pylon_pos=(27,33)
+        self.next_pylon_pos = (27, 33)
 
     def init(self):
 
@@ -68,12 +69,12 @@ class Core(object):
         try:
             os.system(self.launcher_path)
 
-            time.sleep(5) # need time to connect after launch app.
+            time.sleep(5)  # need time to connect after launch app.
 
         except:
             logger.error("Failed to open sc2.")
 
-        #connection between core and sc2_client using sc2 protobuf.
+        # connection between core and sc2_client using sc2 protobuf.
         self.comm_sc2.open()
 
     def deinit(self):
@@ -82,7 +83,7 @@ class Core(object):
 
     '''
         Collection of Requests to SC2 client.
-        
+
             Includes,
             - _start_new_game
                     After starting SC2 client, to start the game, we have to select the map and request to open the game.
@@ -101,6 +102,7 @@ class Core(object):
                     Update some data 'dict_probe', 'dict_pylon', 'dict_nexus'.
                     Return the tuple that includes 'the amount of minerals', 'food capacity', 'food used'
     '''
+
     def _start_new_game(self):
 
         # create a game
@@ -120,7 +122,7 @@ class Core(object):
 
             logger.info('New game is created.')
         except Exception as ex:
-            logger.error('While creating a new game: %s'%str(ex))
+            logger.error('While creating a new game: %s' % str(ex))
 
         # join the game
         try:
@@ -132,15 +134,15 @@ class Core(object):
 
             logger.info('Success to join the game.')
         except Exception as ex:
-            logger.error('While joining the game: %s'%str(ex))
+            logger.error('While joining the game: %s' % str(ex))
 
         # Game Start
         try:
-            #print(self.comm_sc2.send(step=sc_pb.RequestStep(count=1)))
+            # print(self.comm_sc2.send(step=sc_pb.RequestStep(count=1)))
 
             logger.info('Game is Started.')
         except Exception as ex:
-            logger.error('While starting a new game: %s'%str(ex))
+            logger.error('While starting a new game: %s' % str(ex))
 
     def _leave_game(self):
         print(self.comm_sc2.send(leave_game=sc_pb.RequestLeaveGame()))
@@ -176,14 +178,14 @@ class Core(object):
 
         for unit in t.observation.observation.raw_data.units:
 
-            if unit.unit_type == 84: # Probe tag
+            if unit.unit_type == 84:  # Probe tag
 
                 # Already exists
                 if unit.tag in self.dict_probe:
                     self.dict_probe[unit.tag] = (unit.pos.x, unit.pos.y, unit.pos.z)
                 else:
                     # new probe
-                    if self.spawned_agent>=4:
+                    if self.spawned_agent >= 4:
                         continue
 
                     self.dict_probe[unit.tag] = (unit.pos.x, unit.pos.y, unit.pos.z)
@@ -192,19 +194,19 @@ class Core(object):
                     self.threads_agents.append(Agent())
 
                     # If the agent have to know their name
-                    #send_knowledge={}
-                    #send_knowledge.update(self.initial_knowledge)
-                    #send_knowledge.update({''})
+                    # send_knowledge={}
+                    # send_knowledge.update(self.initial_knowledge)
+                    # send_knowledge.update({''})
 
                     self.threads_agents[-1].spawn(unit.tag, 84,
-                                        initial_knowledge = self.initial_knowledge,
-                                        initial_goals = [create_goal_set(self.goal)]
-                                    )
+                                                  initial_knowledge=self.initial_knowledge,
+                                                  initial_goals=[create_goal_set(self.goal)]
+                                                  )
 
                     self.threads_agents[-1].start()
-                    self.spawned_agent+=1
+                    self.spawned_agent += 1
 
-            if unit.unit_type == 341: # Mineral tag
+            if unit.unit_type == 341:  # Mineral tag
 
                 # Already exists
                 if unit.tag in self.dict_mineral:
@@ -224,14 +226,13 @@ class Core(object):
         minerals = t.observation.observation.player_common.minerals
         food_cap = t.observation.observation.player_common.food_cap
         food_used = t.observation.observation.player_common.food_used
-        print('Minerals: ',minerals)
-        print('Population: %d/%d'%(food_used,food_cap))
-        return (minerals,food_cap,food_used)
-
+        print('Minerals: ', minerals)
+        print('Population: %d/%d' % (food_used, food_cap))
+        return (minerals, food_cap, food_used)
 
     '''
         Connection methods to broadast and receive msg.
-        
+
             Includes,
             - _start_proxy
                     Start the proxy that is broker among the agents, also between core and agents.
@@ -240,13 +241,14 @@ class Core(object):
             - perceive_request
                     To get requests from agents, such as to move probe, to gather minerals.
     '''
+
     def _start_proxy(self):
         logger.info("Try to turn on proxy...")
         self.thread_proxy.start()
-        time.sleep(1) # wait for turn on proxy
+        time.sleep(1)  # wait for turn on proxy
 
-    def broadcast(self,msg):
-        self.comm_agents.send(msg,broadcast=True)
+    def broadcast(self, msg):
+        self.comm_agents.send(msg, broadcast=True)
 
     def perceive_request(self):
         return self.comm_agents.read()
@@ -255,52 +257,79 @@ class Core(object):
         observation = sc_pb.RequestObservation()
         t = self.comm_sc2.send(observation=observation)
 
-        list_minerals=[]
+        list_minerals = []
 
         for unit in t.observation.observation.raw_data.units:
             if unit.unit_type == 341:  # Mineral tag
                 list_minerals.append(unit.tag)
 
-
-
-        self.goal = {'goal': 'I have Pylon',
+        self.goal = {'goal': 'I have two Pylon',
                      'trigger': [],
                      'satisfy': [
                          ('type2', 'i', 'have', ['100 minerals'])
                      ],
                      'precedent': [],
                      'require': [
-                         {'goal': 'gather 100 minerals',
-                            'require': [
-                                ['gather 1', {'target': 'unit', 'unit_tag': list_minerals[0]}, 'General'],
-                                # target: unit
-                                ['gather 2', {'target': 'unit', 'unit_tag': list_minerals[0]}, 'General'],
-                                ['gather 3', {'target': 'unit', 'unit_tag': list_minerals[0]}, 'General'],
-                                ['gather 4', {'target': 'unit', 'unit_tag': list_minerals[0]}, 'General'],
-                                ['check mineral 1', {'target': 'minerals', 'amount': 9000}, 'Query'],
-                                # ['check mineral 2', {'target': 'minerals', 'amount': 20}, 'Query'],
-                            ]
-                         },
-                         ['build_pylon', {'target': 'point', 'pos_x': 39, 'pos_y': 50 }, 'General'],
+                         {'goal': 'I have pylon 1',
+                          'require': [
+                              {'goal': 'gather 100 minerals 1',
+                               'require': [
+                                   ['gather 1', {'target': 'unit', 'unit_tag': list_minerals[0]}, 'General'],
+                                   # target: unit
+                                   ['gather 2', {'target': 'unit', 'unit_tag': list_minerals[0]}, 'General'],
+                                   ['gather 3', {'target': 'unit', 'unit_tag': list_minerals[0]}, 'General'],
+                                   ['gather 4', {'target': 'unit', 'unit_tag': list_minerals[0]}, 'General'],
+                                   ['check mineral 1', {'target': 'minerals', 'amount': 100}, 'Query'],
+                                   # ['check mineral 2', {'target': 'minerals', 'amount': 20}, 'Query'],
+                               ]
+                               },
+                              ['build_pylon 1', {'target': 'point', 'pos_x': 39, 'pos_y': 29}, 'General'],
+                          ]
+                          },
+                         {'goal': 'I have pylon 2',
+                          'require': [
+                              {'goal': 'gather 100 minerals 2',
+                               'require': [
+                                   ['gather 5', {'target': 'unit', 'unit_tag': list_minerals[0]}, 'General'],
+                                   # target: unit
+                                   ['gather 6', {'target': 'unit', 'unit_tag': list_minerals[0]}, 'General'],
+                                   ['gather 7', {'target': 'unit', 'unit_tag': list_minerals[0]}, 'General'],
+                                   ['gather 8', {'target': 'unit', 'unit_tag': list_minerals[0]}, 'General'],
+                                   ['check mineral 2', {'target': 'minerals', 'amount': 150}, 'Query'],
+                               ]
+                               },
+                              ['build_pylon 2', {'target': 'point', 'pos_x': 39, 'pos_y': 27}, 'General'],
+                          ]
+                          },
+
                      ]
+
                      }
 
     def set_init_kn(self):
-        self.initial_knowledge =    { self.goal['goal'] : { 'is' : 'Not Assigned' },
-                                      'gather 100 minerals' : {'is' : 'Not Assigned'},
-                                      'gather 1' : { 'is' : 'Ready' },
-                                      'gather 2' : { 'is' : 'Ready' },
-                                      'gather 3' : { 'is' : 'Ready' },
-                                      'gather 4' : { 'is' : 'Ready' },
-                                      'build_pylon': { 'is' : 'Ready'},
-                                      'check mineral 1' : { 'is' : 'Ready'},
-                                      #'check mineral 2' : { 'is' : 'Ready'}
-
-                                    }
+        self.initial_knowledge = {self.goal['goal']: {'is': 'Not Assigned'},
+                                  'gather 100 minerals 1': {'is': 'Not Assigned'},
+                                  'gather 100 minerals 2': {'is': 'Not Assigned'},
+                                  'I have pylon 1': {'is': 'Not Assigned'},
+                                  'I have pylon 2': {'is': 'Not Assigned'},
+                                  'gather 1': {'is': 'Ready'},
+                                  'gather 2': {'is': 'Ready'},
+                                  'gather 3': {'is': 'Ready'},
+                                  'gather 4': {'is': 'Ready'},
+                                  'gather 5': {'is': 'Ready'},
+                                  'gather 6': {'is': 'Ready'},
+                                  'gather 7': {'is': 'Ready'},
+                                  'gather 8': {'is': 'Ready'},
+                                  'build_pylon 1': {'is': 'Ready'},
+                                  'build_pylon 2': {'is': 'Ready'},
+                                  'check mineral 1': {'is': 'Ready'},
+                                  'check mineral 2': {'is': 'Ready'},
+                                  }
 
     '''
         The Main Part of Core.
     '''
+
     def run(self):
 
         self._start_new_game()
@@ -312,23 +341,23 @@ class Core(object):
 
             logger.info('%s is ticking' % ('core'))
 
-            minerals,food_cap,food_used=self._req_playerdata()
+            minerals, food_cap, food_used = self._req_playerdata()
 
             # Tell game data to everyone.
             data = {}
-            data['minerals']={}
+            data['minerals'] = {}
             data['minerals']['gathered'] = str(minerals)
-            data['minerals']['are']=list(self.dict_mineral.items())
-            data['food']={}
+            data['minerals']['are'] = list(self.dict_mineral.items())
+            data['food'] = {}
             data['food']['has'] = str(food_cap)
             data['food']['used'] = str(food_used)
-            #data['probes']={'are':self.dict_probe.items()}
-            #data['nexus']={'are':self.dict_nexus.items()}
+            # data['probes']={'are':self.dict_probe.items()}
+            # data['nexus']={'are':self.dict_nexus.items()}
 
-            json_string=json.dumps(data)
+            json_string = json.dumps(data)
             self.broadcast(json_string)
 
-            if minerals>=10500: # End option <- Should be delete
+            if minerals >= 500:  # End option <- Should be delete
 
                 # Should be delete! Cause when the goal is achieved, the agent destroy itself.
                 for probe in self.threads_agents:
@@ -341,15 +370,15 @@ class Core(object):
             # Get Requests from agents.
 
             for i in range(len(self.threads_agents)):
-                req=self.perceive_request()
+                req = self.perceive_request()
                 if req.startswith('core'):
-                    req=req[5:]
-                    req=json_format.Parse(req,sc_pb.RequestAction())
-                    #json.loads(req)
+                    req = req[5:]
+                    req = json_format.Parse(req, sc_pb.RequestAction())
+                    # json.loads(req)
                     self.comm_sc2.send(action=req)
 
-            #TODO : Randomly Occured Error...
-            #self._train_probe(list(self.dict_nexus.keys())[0])
+            # TODO : Randomly Occured Error...
+            # self._train_probe(list(self.dict_nexus.keys())[0])
 
             time.sleep(0.5)
 
